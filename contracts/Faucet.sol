946db85e60fee57f4d7bfd26c81d8c11f1cb73e5
Cesar Lopez <clopez@logicasoft.io>
2021-08-05 16:13:23 -0600

    Events added

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.4.22 <0.9.0;
 import './Owner.sol';
 
 contract Faucet is Owner {
+
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed to, uint amount);
+
     function withdraw(uint amount) public {
         // check that amount is max 0.1 eth
         require(amount <= 0.1 ether);
@@ -11,7 +15,9 @@ contract Faucet is Owner {
         // sender = the one who triggers the contract
         // this statement transfers the required amount to
         // the sender
-        owner.transfer(amount);
+        payable(msg.sender).transfer(amount);
+
+        emit Withdrawal(msg.sender, amount);
     }
 
     // receive any amount
